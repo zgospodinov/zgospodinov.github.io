e2fd4a4515e2e3677b56a518c9d1914d861ab81a
zdravki4 <zdravko.gospodinov@gmail.com>
2018-11-11 11:20:21 +0200

    Restore
    Restoring previous state

---
+++ js/zground.js
@@ -0,0 +1,10 @@
+function myResizeCallback() {
+    // var w = window.outerWidth;
+    // var h = window.outerHeight;
+    // var txt = "Window size: width=" + w + ", height=" + h;
+    // console.log(txt)
+
+    var clHeight = window.document.clientHeight;
+    console.log(clHeight);
+    
+};

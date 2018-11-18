a2007a7c5c41d35de1dea1b6b2ee38965eac073c
zdravki4 <zdravko.gospodinov@gmail.com>
2018-11-18 22:18:45 +0200

    Certificates
    Testing certificates thumbnails

--- js/zground.js
+++ js/zground.js
@@ -1,10 +1,20 @@
-function myResizeCallback() {
-    // var w = window.outerWidth;
-    // var h = window.outerHeight;
-    // var txt = "Window size: width=" + w + ", height=" + h;
-    // console.log(txt)
+// Get the modal
+var modal = document.getElementById('myModal');
 
-    var clHeight = window.document.clientHeight;
-    console.log(clHeight);
-    
-};
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
